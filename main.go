2a68fbbcf9c2db0b422742eae08e1b18812e65d5
NitroSniper <mahiemiah35@gmail.com>
2024-09-08 21:19:35 +0100

    feat: markdown now served on a webpage

--- main.go
+++ main.go
@@ -3,24 +3,75 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"github.com/yuin/goldmark"
+	"html/template"
+	"log"
+	"net/http"
 	"os"
+
+	"github.com/yuin/goldmark"
+	"github.com/yuin/goldmark/extension"
+	"github.com/yuin/goldmark/parser"
+	"github.com/yuin/goldmark/renderer/html"
 )
 
-func e(err error) {
-	if err != nil {
+func hello(w http.ResponseWriter, req *http.Request) {
+	buf := foo()
+
+	templ := template.Must(template.New("index").Parse(`
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<title>Exmpale</title>
+	</head>
+	<body>
+		{{.Markdown}}
+	</body>
+	</html>
+`))
+	data := struct {
+		Markdown template.HTML
+	}{
+		Markdown: template.HTML(buf.Bytes()),
+	}
+
+	if err := templ.Execute(w, data); err != nil {
 		panic(err)
 	}
 }
 
-func main() {
-	source, err := os.ReadFile("./example.md")
-	e(err)
+func headers(w http.ResponseWriter, req *http.Request) {
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
+	}
+}
 
-	var buf bytes.Buffer
-	if err := goldmark.Convert(source, &buf); err != nil {
+func foo() bytes.Buffer {
+	source, err := os.ReadFile("./example.md")
+	if err != nil {
 		panic(err)
 	}
+	md := goldmark.New(
+		goldmark.WithExtensions(extension.GFM),
+		goldmark.WithParserOptions(
+			parser.WithAutoHeadingID(),
+		),
+		goldmark.WithRendererOptions(
+			html.WithHardWraps(),
+			html.WithXHTML(),
+		),
+	)
+	var buf bytes.Buffer
+	if err := md.Convert(source, &buf); err != nil {
+		panic(err)
+	}
+	return buf
+}
 
-	fmt.Print(string(buf.Bytes()))
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/hello", hello)
+	mux.HandleFunc("/headers", headers)
+	log.Fatal(http.ListenAndServe(":8090", mux))
 }
